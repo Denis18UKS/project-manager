054dd62cefdbf9be7c8cfc5bb502f2f49b30a36f
KhamitovDanil <l123kira123l@gmail.com>
2024-10-12 11:10:51 +0500

    Progect page

--- project-manager-backend/bootstrap/cache/packages.php
+++ project-manager-backend/bootstrap/cache/packages.php
@@ -1,11 +1,4 @@
 <?php return array (
-  'fruitcake/laravel-cors' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
-    ),
-  ),
   'laravel/sail' => 
   array (
     'providers' => 
@@ -25,13 +18,6 @@
     'providers' => 
     array (
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
-    ),
-  ),
-  'laravel/ui' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Ui\\UiServiceProvider',
     ),
   ),
   'nesbot/carbon' => 
